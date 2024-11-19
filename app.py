635da6eeecedc8a9e64a3fe47e0cafbb818dd549
saiprasanth20 <125905158+saiprasanth20@users.noreply.github.com>
2024-11-19 20:52:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,10 +61,7 @@ def healthcheck():
     return jsonify({'status': 'API is running successfully!'})
 
 # Run the Flask app
-
-
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
